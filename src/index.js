3d18d14cff5d3b39f9c25a38520bc97f8dec010f
Pascal Weber <weber-pascal@web.de>
2021-04-30 21:20:49 +0200

    java stand2

--- src/index.js
+++ src/index.js
@@ -29,9 +29,11 @@ bookmark.addEventListener('click', () => {
 
 const darkmode = document.querySelector('.darkmode')
 const body = document.querySelector('body')
+const t2 = document.querySelector('.t2')
 
 darkmode.addEventListener('click', () => {
   body.classList.toggle('mode')
+  t2.classList.toggle('t1')
 })
 
 buttonHome.addEventListener('click', () => {
